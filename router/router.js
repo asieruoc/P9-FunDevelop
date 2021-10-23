095d5f6c3d81cf3d138140f8a0177a8dfc538de7
jmoletr <jmoler@uoc.edu>
2021-10-24 00:38:42 +0200

    Front index

--- router/router.js
+++ router/router.js
@@ -22,6 +22,16 @@ exports.init = function(req, res){
                 res.end(data);
             }
         }); 
+    }else if(path==="/salas"){
+        res.setHeader('Content-Type', HTML_CONTENT_TYPE);
+        const index=fs.readFile(__dirname + '/../public/salas.html', (err, data) => {
+            if(err){
+                console.log("Error en la carga del salas.html");
+                res.end("Error en la carga del salas.html");
+            }else{
+                res.end(data);
+            }
+        }); 
     }else if(path.match("\.css$")){
         res.setHeader('Content-Type', CSS_CONTENT_TYPE);
         const css=fs.readFile(__dirname + '/../public/' + path, (err, data) => {
