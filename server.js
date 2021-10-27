2b33599df45f613d2edaa1886c46e9d32e980329
Paki <frodriguezvaz@uoc.edu>
2021-10-27 14:06:22 +0200

    Responsive + acceso sala + value avatar

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ const port = serverConfig['port'];
 //Ante cualquier request devolveremos las siguientes respuestas:
 const server = http.createServer((req, res) => {
     router.init(req,res);
-<<<<<<< 863579243680f8b30a645d6e6d102c8c3032dcd6
-=======
+
 
     // we can access HTTP headers
   req.on('data', data => {
@@ -29,7 +28,7 @@ const server = http.createServer((req, res) => {
     }
   })
 
->>>>>>> Imagen fondo responsive y acceso a salas
+
 });
 
 //Lanzar servidor
