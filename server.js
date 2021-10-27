422eb531a53b951558f9c147e2554e64fe7aee6a
Paki <frodriguezvaz@uoc.edu>
2021-10-27 13:43:55 +0200

    Imagen fondo responsive y acceso a salas

--- server.js
+++ server.js
@@ -19,6 +19,17 @@ const port = serverConfig['port'];
 //Ante cualquier request devolveremos las siguientes respuestas:
 const server = http.createServer((req, res) => {
     router.init(req,res);
+<<<<<<< 863579243680f8b30a645d6e6d102c8c3032dcd6
+=======
+
+    // we can access HTTP headers
+  req.on('data', data => {
+    if(req.url === "/salas"){
+      console.log("Almacenar datos del jugador " + data);
+    }
+  })
+
+>>>>>>> Imagen fondo responsive y acceso a salas
 });
 
 //Lanzar servidor
