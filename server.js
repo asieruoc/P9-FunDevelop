51771ded2e13be8f2aac5fa703695d796ec07b71
jmoletr <jmoler@uoc.edu>
2021-11-17 22:10:51 +0100

    Instalación de Express.js y librerias o Middlewares bodyParser y nodemon. Modificiación del fichero server.js

--- server.js
+++ server.js
@@ -1,35 +1,25 @@
-//Carga de módulos o librerias externas
-//Módulo interno de Node. Nos ayuda a realizar la interacción con las peticiones request y response.
-const http = require('http');
-const fs = require('fs');
-//Módulo que hemos generado nosotros.
-const router = require('./router/router.js');
+//https://www.youtube.com/watch?v=64QLTzkDPio&list=PLUdlARNXMVkk7E88zOrphPyGdS50Tadlr&index=4
 
-//Configuraciones del servidor
-//Guardamos en la variable serverConfigFile la lectura del fichero env.json en texto plano.
-const serverConfigFile = fs.readFileSync(__dirname + '/config/env.json', 'utf8');
-//Parseamos el fichero y lo guardamos en la variable serverConfig.
-const serverConfig = JSON.parse(serverConfigFile);
-//Damos el valor que corresponde en cada uno de sus variables.
-const hostname = serverConfig['hostname'];
-const port = serverConfig['port'];
+//para utilizar nuevas funcionalidades
+'use strict'
 
-//Crear servidor
-//Peticiones Request (via navegador), Devolvemos una respuesta (res)
-//Ante cualquier request devolveremos las siguientes respuestas:
-const server = http.createServer((req, res) => {
-    router.init(req,res);
-    // we can access HTTP headers
-    req.on('data', data => {
-      if(req.url === "/salas"){
-        console.log("Almacenar datos del jugador " + data);
-      }
-    })
-});
+//Importar modulo express
+const express = require('express')
+//Importar modulo bodyParser (nos permite parsear el cuerpo de la petición (Get,Post))
+//Express funciona con middleware, que son como diferentes capas que se van añadiendo y cada vez que se realice una petición HTTP
+//va a pasar por esas distintas capas.
+const bodyParser = require('body-parser')
 
-//Lanzar servidor
-server.listen(port, hostname, () => {
-  console.log(`Servidor corriendo en http://${hostname}:${port}/`);
-});
+//creamos variable app que llama a express.
+const app = express()
+const port = process.env.PORT || 3000
 
-// Capturar usuario y avatar
+//añadimos los middlewares con el metodo use con la propiedad extended
+app.use(bodyParser.urlencoded({ extended: false}))
+//Para poder admitir peticiones con el cuerpo de mensaje en JSON
+app.use(bodyParser.json())
+
+//app escucha  el puerto 3000 (=> es igual a function())
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+})
