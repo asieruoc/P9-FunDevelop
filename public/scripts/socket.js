4fa0a7720ffea91ab0a8899ec879b1bf61b39bd8
PakiRV <frodriguezvaz@uoc.edu>
2021-12-03 13:06:20 +0100

    Nuevas funcionalidades del juego

--- public/scripts/socket.js
+++ public/scripts/socket.js
@@ -5,34 +5,13 @@ socket.on("connection");
 socket.on(salaId, (data) => {
     const {action, player, position, sala } = data;
     if(action === "newPlayer"){
-        document.getElementById(sala.id).innerHTML = "";
-        sala.players.forEach(player => {
-            var li = document.createElement("li");
-            li.id = player.id;
-            var img = document.createElement("img");
-            img.src = "/img/Avatar"+player.avatar+".png";
-            li.append(img);
-            li.append(player.username);
-            document.getElementById(sala.id).append(li);
-        })
+        reloadSalaInfo(sala);
     }
     else{
-        document.querySelectorAll(".cell")[position].innerHTML = player.username;
-        document.querySelectorAll(".cell")[position].setAttribute("disabled", true);
+        seleccionarCelda(position, player.username);
     }
 });
 
-function buttonListeners() {
-    document.querySelectorAll(".cell").forEach((button, i) =>
-    button.addEventListener("click", () => {
-        var userData = JSON.parse(localStorage.getItem("userData"));
-        let data = {
-            player: userData,
-            position: i,
-        }
-        socket.emit(salaId, data)
-    }))
-}
 
 socket.emit(salaId, {action: "newPlayer"})
 
